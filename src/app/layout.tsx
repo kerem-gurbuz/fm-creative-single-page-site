4e88591c4e4bdc2ed61d2b7e5677f1203cb56008
Kerem Gurbuz <grbz.kerem@gmail.com>
2024-11-12 22:28:46 +0300

    feat: enable smooth scrolling within elements using Tailwind CSS utility
    Added `scroll-smooth` utility to `<html>` tag for smoother user experience when navigating within the page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${commissioner.variable} font-commissioner antialiased`}
       >
