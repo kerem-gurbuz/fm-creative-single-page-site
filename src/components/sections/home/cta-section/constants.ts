df3e269e42976428972bc7e5deda8fb94df7aaf3
Kerem Gurbuz <grbz.kerem@gmail.com>
2024-11-16 13:20:16 +0300

    feat(constants): add CTA section identifiers
    - Add SECTION_ID constant
    - Add SECTION_HEADING_ID constant

---
+++ src/components/sections/home/cta-section/constants.ts
@@ -0,0 +1,6 @@
+/* -------------------------------------------------------------------------- */
+/*                            Cta Section Constants                           */
+/* -------------------------------------------------------------------------- */
+
+export const SECTION_ID = 'cta-section';
+export const SECTION_HEADING_ID = 'cta-heading';
